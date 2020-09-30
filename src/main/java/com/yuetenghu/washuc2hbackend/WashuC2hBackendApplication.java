003c518b849399adb0b1b492e37d5ef7cf88f777
Yueteng HU <yuetenghu@Yue-Mac.local>
2020-09-30 00:38:51 -0400

    Base code with Spring Initializr and github/gitignore

---
+++ src/main/java/com/yuetenghu/washuc2hbackend/WashuC2hBackendApplication.java
@@ -0,0 +1,13 @@
+package com.yuetenghu.washuc2hbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WashuC2hBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WashuC2hBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yuetenghu/washuc2hbackend/WashuC2hBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yuetenghu.washuc2hbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WashuC2hBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
